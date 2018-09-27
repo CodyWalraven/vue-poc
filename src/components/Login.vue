c9a0c1d1c87986f26724480ed6cea65fa215d1e2
toasttify <ipod22433@gmail.com>
2018-09-27 14:52:04 -0500

    initial login screen

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="bg">
-    <img class="images" src="../assets/giant_text.png"> 
+    <div class="image-container">
+      <img class="images" src="../assets/text-rectangle-square.png"> 
+    </div>
     
-<div class="container">
+<div class="container main-container">
   <input type="text" placeholder="email" class="input">
   <br>
   <input type="password" placeholder="password" class="input">
@@ -14,6 +16,11 @@
 
 <script lang="ts">
 import Vue from "vue"
+import BootstrapVue from "bootstrap-vue"
+
+Vue.use(BootstrapVue)
+import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap-vue/dist/bootstrap-vue.css"
 
 export default Vue.extend({
   name: "HelloWorld",
@@ -30,35 +37,17 @@ h3 {
 }
 
 .images {
-  max-width: 40%;
-
-  min-height: 30%;
-  height: auto;
-  width: auto;
-}
-ul {
-  list-style-type: none;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+  max-width: 60%;
+  max-height: 60%;
+  margin-top: 4rem;
+  margin-bottom: 4.5rem;
 }
 
-p {
-  color: black;
-  font-size: 100px;
-  font-family: ubuntu;
-  text-align: center;
-  text-shadow: 4px 4px 0 rgba(0, 0, 0, 0.1);
-}
 .bg {
-  background: linear-gradient(45deg, #30496b, #30b8d2);
-  flex: 1;
-  flex-direction: column;
+  background: linear-gradient(35deg, #00b4db, #336699);
+  padding-bottom: 400px;
 }
+
 .button {
   background: #009933;
   align-self: flex-end;
@@ -66,31 +55,39 @@ p {
   max-width: 400px;
   margin: 0 auto;
   border-radius: 20px;
-  font-size: 2rem;
+  color: white;
+  padding: 5px 0px 5px;
+  font-size: 2.4rem;
   font-family: ubuntu;
   text-align: center;
-  text-shadow: 2px 2px 0 rgba(0, 0, 0, 0.1);
-  padding: 1.4rem 1rem 1.6rem;
+  text-shadow: 2px 2px 0 rgba(29, 10, 10, 0.1);
+  border: none;
+}
+
+.main-container {
+  flex-direction: column;
+  flex: 1;
+  justify-content: flex-end;
 }
 
 input {
   background: rgba(255, 255, 255, 0.5);
-  align-self: flex-end;
   width: 100%;
+  color: black;
+  font-size: 1.2rem;
   max-width: 400px;
-  padding-bottom: 10;
   margin: 0 auto;
   border-radius: 20px;
-  padding: 1.4rem 1rem 1.6rem;
+  padding: 1.4rem 1rem 1.2rem;
+  border: none;
 }
 
 .container {
   display: flex;
   flex-direction: column;
-  // justify-content: center;
-  height: 100vh;
+  max-height: auto;
   max-width: 1600px;
-  padding: 0 15px;
+  padding: 10px 15px 300px;
   margin: 0 auto;
 }
 </style>
